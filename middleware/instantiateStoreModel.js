78e9f31e3fa7e1ddc8ad0871a98b871a819d3fce
Salmaan Ahmed <salmaanshaik72@gmail.com>
2018-02-15 16:42:18 +0530

    Update instantiateStoreModel.js

--- middleware/instantiateStoreModel.js
+++ middleware/instantiateStoreModel.js
@@ -10,18 +10,14 @@ let instantiateStoreModel = {};
 instantiateStoreModel.instantiate = function(req,res,next){
   verifyToken.verifyUserToken(req.headers.cookie,function(authData){
     mongoose.connection.db.listCollections({name:"storemodels"}).toArray(function(err, arr){
-      console.log('1')
+      
       if(arr.length > 0) {
-        console.log('2')
         storeModel.findOne({storeId:"store-456"},function(err,store){
-          console.log('3')
           res.mydata = store._id;
-          console.log('got here');
           next();
         });
       }
       else if(arr.length === 0){
-        console.log('4');
         let newstore = new storeModel({
           electronics:{
             mobiles: [],
@@ -40,7 +36,6 @@ instantiateStoreModel.instantiate = function(req,res,next){
           },
           storeId: "store-456"
         });
-        console.log('5');
         newstore.save().then(function(sr){
           res.mydata = sr._id;
           next();
